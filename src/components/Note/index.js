279f189c3d9f6f31888af424891fbc37e6b4003d
Purnima <pooyax14@gmail.com>
2020-02-25 01:40:33 +0530

    show pin icon when hovered on grid-item

--- src/components/Note/index.js
+++ src/components/Note/index.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { TiPinOutline } from 'react-icons/ti';
 import './index.css';
 
 function Note(props) {
+    const [hover, setHover] = useState(false);
     const { id, title, description, isPinned } = props.note;
     
     function handleClick(){
         props.onClick({ id, title, description, isPinned });
     }
 
+    function onHover() {
+        setHover(true)
+    }
+
+    function onHoverLeave() {
+        setHover(false)
+    }
+    
     return (
-        <div className={`grid-item ${props.size}`} onClick={handleClick}>
-            <strong>{title}</strong>
+        <div className={`grid-item ${props.size} ${hover ? "show_tools" : ""}`} 
+            onClick={handleClick} 
+            onMouseEnter={onHover}
+            onMouseLeave={onHoverLeave}
+            >
+            {/* <strong>{title}</strong>   */}
+            <div className="note-card-heading__container">
+                <div className="note-card__title">{title}</div>  
+                <div className="pin_note">
+                    <TiPinOutline size="30"/>
+                </div>
+            </div>
+            
+                
             <p>{description}</p>
+           
         </div>
     )
 }
 
 export default Note;
+
+// className={`grid-item ${props.size}`}
